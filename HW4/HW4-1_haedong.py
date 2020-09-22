e4553ba9a8683e8f09ea19a5bde36e2d67c456e5
Haedong Kim - Mac <haedong.kim90@gmail.com>
2020-09-21 23:55:38 -0400

    HW4

--- HW4/HW4-1_haedong.py
+++ HW4/HW4-1_haedong.py
@@ -18,6 +18,15 @@ def obj_fn(x1, x2):
     return J
 
 def gradient_fn(x1, x2):
+    """calculate gradient
+
+    Args:
+        x1 (numeric): 1st dim
+        x2 (numeric): 2nd dim
+
+    Returns:
+        list: gradient at current point
+    """
     wrt_x1 = -4*np.power(x2-x1, 3) + 8*x2 - 1
     wrt_x2 = 4*np.power(x2-x1, 3) + 8*x1 + 1
     
@@ -25,6 +34,16 @@ def gradient_fn(x1, x2):
     return grads
     
 def gradient_learner(x1, x2, alpha):
+    """gradient learning algorithm
+
+    Args:
+        x1 (numeric): 1st dim
+        x2 (numeric): 2nd dim
+        alpha (numeric): learning rate
+
+    Returns:
+        list: next point
+    """
     grads = gradient_fn(x1, x2)
     
     new_x1 = x1 - alpha*grads[0]
@@ -38,28 +57,52 @@ def gradient_learner(x1, x2, alpha):
 xx1, xx2 = np.meshgrid(np.arange(-1, 1, 0.01), np.arange(-1, 1, 0.01))
 jj = obj_fn(xx1, xx2)
 
-fig1, ax1 = plt.subplots()
+fig1, ax1 = plt.subplots(dpi=300)
 cm = ax1.pcolormesh(xx1, xx2, jj)
 fig1.colorbar(cm, ax=ax1)
 
-fig2, ax2 = plt.subplots()
+fig2, ax2 = plt.subplots(dpi=300)
 cp = ax2.contour(xx1, xx2, jj)
 ax2.clabel(cp, inline=True, fontsize=10)
 
 
 ## problem b to d -----
 # learning parameters
-iters = 50000
-alpha = 0.00000001
-init_pt = [10, -40]
+alpha = 0.01
+init_pt = [0, 0]
+tol = 0.00001
+max_iter = 500
 
+cnt = 1
 pts = list()
-pts.append(init_pt)
-
 fvals = list()
-for i in range(iters):
-    pt = pts[i]
+
+# initialization
+pts.append(init_pt)
+fvals.append(obj_fn(init_pt[0], init_pt[1]))
+pts.append(gradient_learner(init_pt[0], init_pt[1], alpha))
+while True:
+    pt = pts[cnt]
     fvals.append(obj_fn(pt[0], pt[1]))
 
-    pts.append(gradient_learner(pt[0], pt[1], alpha))
-    
+    # check stopping criteria
+    fdiff = abs(fvals[cnt] - fvals[cnt-1])
+    if (fdiff < tol) or cnt==max_iter:
+        break
+    else:
+        pts.append(gradient_learner(pt[0], pt[1], alpha))
+        cnt += 1
+
+x1_search = [x[0] for x in pts]
+x2_search = [x[1] for x in pts]
+
+fig3, ax3 = plt.subplots(dpi=300)
+cp = ax3.contour(xx1, xx2, jj)
+ax3.clabel(cp, inline=True, fontsize=10)
+ax3.scatter(x1_search, x2_search, color='red')
+ax3.plot(x1_search[-1], x2_search[-1], marker='*', color='yellow')
+
+fig4, ax4 = plt.subplots(dpi=300)
+p = ax4.scatter(np.arange(len(fvals)), fvals, s=1)
+plt.xlabel('Iterations')
+plt.ylabel('Objective function')
--- HW4/HW4-2_haedong.py
+++ HW4/HW4-2_haedong.py
@@ -18,20 +18,31 @@ def simulator(n, sig):
     return sim_data
 
 def sse(x, y, b0, b1):
-    # x vector
-    # y vector
     hat = b0 + b1*x
 
-    sse = 1/(2*len(x)) * np.power(y - hat, 2)
+    sse = 1/(2*len(x)) * np.sum(np.power(y - hat, 2))
     return sse
+
+def sse_grid(x, y, bb0, bb1):
+    grid_size = bb0.shape
+
+    ee = np.empty(shape=grid_size)
+    row_size = grid_size[0]
+    col_size = grid_size[1]
+
+    for i in range(row_size):
+        for j in range(col_size):
+            ee[i,j] = sse(x, y, bb0[i,j], bb1[i,j])
+
+    return ee
 
 def gradient_learner(x, y, b0, b1, alpha):
     # estimation
     hat = b0 + b1*x
 
     # calculate derivatives
-    wrt_b0 = 1/len(x) * (hat - y)
-    wrt_b1 = 1/len(x) * np.multiply((hat - y), x)
+    wrt_b0 = 1/len(x) * np.sum(hat - y)
+    wrt_b1 = 1/len(x) * np.sum(np.multiply((hat - y), x))
 
     # update
     new_b0 = b0 - alpha * wrt_b0
@@ -42,29 +53,67 @@ def gradient_learner(x, y, b0, b1, alpha):
 
 
 ## problem a -----
-sim_data = simulator(20, 3)
-plt.scatter(sim_data[:,0], sim_data[:,1])
-
-
-## problem b -----
-tol = 0.01
-alpha = 0.05
-
+sim_data = simulator(20, 1)
 x = sim_data[:,0]
 y = sim_data[:,1]
 
+fig1, ax1 = plt.subplots(dpi=300)
+ax1.scatter(x, y)
+plt.xlabel('x')
+plt.ylabel('y')
+
+
+## problem b -----
+alpha = 0.001
+init_pt = [1, 1]
+tol = 1.5
+max_iter = 1000
+
+cnt = 1
 params = list()
 fvals = list()
 
-params.append([1, 1])
-cnt = 0
+params.append(init_pt)
+e = sse(x, y, init_pt[0], init_pt[1])
+fvals.append(e)
+# fvals.append(sse(x, y, init_pt[0], init_pt[1]))
+params.append(gradient_learner(x, y, init_pt[0], init_pt[1], alpha))
 while True:
     param = params[cnt]
-    fvals.append(sse(x, y, param[0], param[1]))
+    e = sse(x, y, param[0], param[1])
+    fvals.append(e)
+    # fvals.append(sse(x, y, param[0], param[1]))
 
-    if fvals[cnt] < tol:
+    if (fvals[cnt]) < tol or cnt >= max_iter:
         break
     else:
         new_param = gradient_learner(x, y, param[0], param[1], alpha)
         params.append(new_param)
         cnt += 1
+
+
+## problem c -----
+bb0, bb1 = np.meshgrid(np.arange(-5, 5, 0.1), np.arange(-5, 5, 0.1))
+ee = sse_grid(x, y, bb0, bb1)
+
+b0_search = [p[0] for p in params]
+b1_search = [p[1] for p in params]
+
+fig2, ax2 = plt.subplots(dpi=300)
+cp = ax2.contour(bb0, bb1, ee)
+ax2.clabel(cp, inline=True, fontsize=10)
+ax2.scatter(b0_search, b1_search, color='red')
+ax2.plot(b0_search[-1], b1_search[-1], marker='*', color='yellow')
+plt.xlabel('b0 (Intercept)')
+plt.ylabel('b1 (Slope)')
+
+best_param = params[-1]
+x4fit = np.arange(0, 40, 0.01)
+y4fit = best_param[0] + best_param[1]*x4fit
+
+fig3, ax3 = plt.subplots(dpi=300)
+ax3.scatter(x, y)
+ax3.plot(x4fit, y4fit, color='red')
+plt.xlabel('x')
+plt.ylabel('y')
+plt.legend()
